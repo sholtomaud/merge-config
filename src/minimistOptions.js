5d6d44b79b7df5092cf0a0257a1f67c567b33956
Sholto Maud <sholto.maud@gmail.com>
2024-01-07 18:21:08 +1100

    Destructured code to allow for a new minimist options generatoer

---
+++ src/minimistOptions.js
@@ -0,0 +1,137 @@
+const fs = require('fs');
+
+module.exports = function (schemaFile, callback) {
+    // Check if schema file exists
+    if (!fs.existsSync(schemaFile)) {
+        return callback(new Error('Schema file does not exist'));
+    }
+
+    // Load the JSONSchema
+    let schema;
+    try {
+        schema = JSON.parse(fs.readFileSync(schemaFile, 'utf8'));
+    } catch (err) {
+        return callback(err);
+    }
+
+    // Extract the properties from the schema
+    const properties = schema.properties;
+
+    // Create an options object for minimist
+    const options = {
+        alias: {},
+        default: {},
+        string: [],
+        boolean: []
+    };
+
+    // Generate the help text from the JSON Schema
+    const helpText = [];
+
+    // For each property in the schema, add it to the options object
+    for (const property in properties) {
+        const prop = properties[property];
+
+        // If the property has an alias, add it to the alias options
+        if (prop.alias) {
+            options.alias[property] = prop.alias;
+        }
+
+        // If the property has a default value, add it to the default options
+        if (prop.default) {
+            options.default[property] = prop.default;
+        }
+
+        // If the property type is boolean, add it to the boolean options
+        if (prop.type === 'boolean') {
+            options.boolean.push(property);
+        }
+
+        // If the property type is string, add it to the string options
+        if (prop.type === 'string') {
+            options.string.push(property);
+        }
+
+        // Add the property to the help text
+        helpText.push(`--${property}, -${prop.alias}\t${prop.description}`);
+    }
+
+    // Call the callback with null error and the result
+    callback(null, { options, helpText: helpText.join('\n') });
+}
+
+// const fs = require('fs');
+// const Ajv = require('ajv');
+
+// // Map the JSON Schema properties to minimist options
+// const options = {
+//     alias: Object.keys(schema.properties).reduce((aliases, key) => {
+//         if (schema.properties[key].alias) {
+//             aliases[key] = schema.properties[key].alias;
+//         }
+//         return aliases;
+//     }, {}),
+//     default: Object.keys(schema.properties).reduce((defaults, key) => {
+//         if (schema.properties[key].default) {
+//             defaults[key] = schema.properties[key].default;
+//         }
+//         return defaults;
+//     }, {})
+// };
+
+// // Generate the help text from the JSON Schema
+// const helpText = Object.keys(schema.properties).map(key => {
+//     const prop = schema.properties[key];
+//     return `--${key}, -${prop.alias}\t${prop.description}`;
+// }).join('\n');
+
+// // Parse the arguments
+// let args = minimist(process.argv.slice(2), options);
+
+// // If the help option is set, display the help text and exit
+// if (args.h || args.help) {
+//     console.log(helpText);
+//     process.exit(0);
+// }
+
+// // Validate the command-line arguments
+// const ajv = new Ajv();
+// const validate = ajv.compile(schema);
+// let valid = validate(args);
+
+// if (!valid) {
+//     console.error('Invalid command-line arguments:', validate.errors);
+//     process.exit(1);
+// }
+
+// // Now you can use args as before
+
+// // module.exports = function (configFile, schemaFile, argv, callback) {
+// //     // Check if files exist
+// //     if (!fs.existsSync(configFile) || !fs.existsSync(schemaFile)) {
+// //         callback(new Error('Config file or schema file does not exist'));
+// //         return;
+// //     }
+
+// //     // Load the config.json file
+// //     const config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+
+// //     // Merge the config and input
+// //     const mergedConfig = { ...config, ...argv };
+
+// //     // Load the JSONSchema
+// //     const schema = JSON.parse(fs.readFileSync(schemaFile, 'utf8'));
+
+// //     // Create an instance of Ajv
+// //     const ajv = new Ajv();
+
+// //     // Validate the merged config against the JSONSchema
+// //     const isValid = ajv.validate(schema, mergedConfig);
+
+// //     if (!isValid) {
+// //         callback(new Error('Merged config is not valid: ' + JSON.stringify(ajv.errors)));
+// //         return;
+// //     }
+
+// //     callback(null, mergedConfig);
+// // }
--- test/config.test.js
+++ test/config.test.js
@@ -1,13 +1,13 @@
 const test = require('tape');
-const validate = require('../src/index'); // adjust path to your module if needed
+const { mergeConfig } = require('../src/index'); // adjust path to your module if needed
 const minimist = require('minimist');
 const fs = require('fs');
 
-test('validate with config file', function (t) {
+test('mergeConfig with config file', function (t) {
     const argv = minimist([]);
     delete argv._;
 
-    validate('./config/config.json', './config/schema.json', argv, (error, config) => {
+    mergeConfig('./config/config.json', './config/schema.json', argv, (error, config) => {
         t.error(error, 'No error');
         t.ok(config, 'Config returned');
 
