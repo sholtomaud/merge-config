7985e54a6a821731e7a1831c6979ad6453e56b07
Sholto Maud <sholto.maud@gmail.com>
2024-01-08 07:26:15 +1100

    Bump patch version

--- src/mergeConfig.js
+++ src/mergeConfig.js
@@ -2,16 +2,30 @@ const fs = require('fs');
 const Ajv = require('ajv');
 
 module.exports = function (configFile, schemaFile, argv) {
-    // Check if files exist
-    if (!fs.existsSync(configFile) || !fs.existsSync(schemaFile)) {
-        throw new Error('Config file or schema file does not exist');
+
+    delete argv._; // Remove the _ property from the argv object (minimist adds this for arguments without a flag)
+
+    // Check if argv is empty
+    const isArgvEmpty = Object.keys(argv).length === 0;
+
+    // Check if configFile exists
+    const doesConfigFileExist = fs.existsSync(configFile);
+
+    // If argv is empty and configFile does not exist, throw an error
+    if (isArgvEmpty && !doesConfigFileExist) {
+        throw new Error('Config file does not exist and no arguments were provided');
     }
 
-    // Load the config.json file
-    const config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+    // Load the config.json file if it exists
+    const config = doesConfigFileExist ? JSON.parse(fs.readFileSync(configFile, 'utf8')) : {};
 
     // Merge the config and input
     const mergedConfig = { ...config, ...argv };
+
+    // Check if schema file exists
+    if (!fs.existsSync(schemaFile)) {
+        throw new Error('Schema file does not exist');
+    }
 
     // Load the JSONSchema
     const schema = JSON.parse(fs.readFileSync(schemaFile, 'utf8'));
